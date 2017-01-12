bf1a502d20a97a58bf089c1cdb9a108ae83f6a4f
fagle <ifagle@qq.com>
2017-01-12 22:51:26 +0800

    disable autojump plugin, add vim vundle auto install

--- setup.sh
+++ setup.sh
@@ -4,13 +4,17 @@ echo $current_dir
 cd ~
 dirname=.bashrc_backup_$(date "+%Y%m%d%H%M%S")
 mkdir $dirname
-mv .bashrc $dirname
-mv .bash_profile $dirname
-mv .tmux.conf $dirname
+cp .bashrc $dirname
+cp .bash_profile $dirname
+cp .tmux.conf $dirname
+cp .vimrc $dirname
 ln -s $current_dir/cygwin_bashrc .bashrc
 ln -s $current_dir/cygwin_bash_profile .bash_profile
 ln -s $current_dir/tmux.conf .tmux.conf
 ln -s $current_dir/vimrc   .vimrc
-echo todo vundle
-echo todo autojump
+echo install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+echo todo install autojump
+
 echo done
